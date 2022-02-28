3907ac0bf15b116c1ab4a72af05088317fad3e71
Kazuki Awasawa <16559662+kawasawa@users.noreply.github.com>
2022-03-01 00:00:00 +0900

    フレームワーク導入 (React), モックサーバ構築 (Express)

---
+++ app/src/constants/styles.ts
@@ -0,0 +1,6 @@
+/* istanbul ignore file */
+
+export const styles = {
+  intersectionOptions: { rootMargin: '-100px' },
+  hoverOptions: { transition: 'all 0.2s ease', '&:hover': { transform: 'scale(1.05)' } },
+} as const;
