d92e4291e04fac1354daa670678da1cecae944a1
Kazuki Awasawa <16559662+kawasawa@users.noreply.github.com>
2022-05-01 00:00:00 +0900

    モックサーバのセキュリティ対応

--- mock/server/src/routes/dev.ts
+++ mock/server/src/routes/dev.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 
-import { asyncError, boomError, error } from '../api';
+import { asyncError, boomError, csrfToken, error, send } from '../api';
 
 const router = express.Router();
 router.get('/error', error);
 router.get('/error/boom', boomError);
 router.get('/error/async', asyncError);
+router.get('/csrf-token', csrfToken);
+router.post('/send', send);
 
 export const devRouter = router;
