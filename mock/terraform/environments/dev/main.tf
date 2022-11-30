b3d077e8aeab20ad73b01e715c5b7ae1e92b7497
Kazuki Awasawa <16559662+kawasawa@users.noreply.github.com>
2022-12-01 00:00:00 +0900

    モックサーバの IaC 対応 (Terraform)

---
+++ mock/terraform/environments/dev/main.tf
@@ -0,0 +1,61 @@
+locals {
+  prefix = "${var.project_short_name}-${var.environment}"
+}
+
+module "network" {
+  source = "../../modules/network"
+  prefix = local.prefix
+}
+
+module "load_balancer" {
+  source                      = "../../modules/load_balancer"
+  prefix                      = local.prefix
+  environment                 = var.environment
+  vpc_main_id                 = module.network.vpc_main_id
+  subnet_public_a_id          = module.network.subnet_public_a_id
+  subnet_public_c_id          = module.network.subnet_public_c_id
+  subnet_private_a_cidr_block = module.network.subnet_private_a_cidr_block
+  subnet_private_c_cidr_block = module.network.subnet_private_c_cidr_block
+  app_port                    = var.app_port
+  domain                      = var.domain
+  dns_zone_id                 = var.dns_zone_id
+  acm_arn                     = var.acm_arn
+}
+
+module "database" {
+  source                      = "../../modules/database"
+  prefix                      = local.prefix
+  environment                 = var.environment
+  vpc_main_id                 = module.network.vpc_main_id
+  subnet_public_a_id          = module.network.subnet_public_a_id
+  subnet_private_a_id         = module.network.subnet_private_a_id
+  subnet_private_c_id         = module.network.subnet_private_c_id
+  subnet_private_a_cidr_block = module.network.subnet_private_a_cidr_block
+  subnet_private_c_cidr_block = module.network.subnet_private_c_cidr_block
+  security_group_ecs_id       = module.container.security_group_ecs_id
+  db_port                     = var.db_port
+  db_name                     = var.db_name
+  db_username                 = var.db_username
+  db_password                 = var.db_password
+}
+
+module "container" {
+  source                      = "../../modules/container"
+  prefix                      = local.prefix
+  environment                 = var.environment
+  vpc_main_id                 = module.network.vpc_main_id
+  subnet_private_a_id         = module.network.subnet_private_a_id
+  subnet_private_c_id         = module.network.subnet_private_c_id
+  subnet_private_a_cidr_block = module.network.subnet_private_a_cidr_block
+  subnet_private_c_cidr_block = module.network.subnet_private_c_cidr_block
+  security_group_alb_id       = module.load_balancer.security_group_alb_id
+  alb_target_group_app_arn    = module.load_balancer.alb_target_group_app_arn
+  app_port                    = var.app_port
+  db_port                     = var.db_port
+  db_name                     = var.db_name
+  db_username                 = var.db_username
+  db_password                 = var.db_password
+  db_address                  = module.database.db_instance_main_address
+  min_container_count         = 1
+  max_container_count         = 3
+}
---
+++ mock/terraform/environments/dev/variables.tf
@@ -0,0 +1,96 @@
+# ******************************************************************************
+# 共通変数
+# ******************************************************************************
+
+variable "project_short_name" {
+  default     = "kawasawa"
+  type        = string
+  description = "プロジェクトの短縮名"
+}
+
+variable "project_name" {
+  default     = "kawasawa.github.io"
+  type        = string
+  description = "プロジェクトの名称"
+}
+
+variable "project_owner" {
+  default     = "Kazuki Awasawa"
+  type        = string
+  description = "プロジェクトの所有者"
+}
+
+variable "domain" {
+  default     = "kawasawa.com"
+  type        = string
+  description = "プロジェクトが使用するドメイン名"
+}
+
+variable "region" {
+  default     = "ap-northeast-1"
+  type        = string
+  description = "AWS のリージョン"
+}
+
+variable "acm_arn" {
+  default     = "arn:aws:acm:ap-northeast-1:123456789012:certificate/12345678-90ab-cdef-1234-567890abcdef"
+  type        = string
+  description = "SSL 証明書を発行した ACM の ARN"
+}
+
+# ******************************************************************************
+# 環境変数
+# ******************************************************************************
+
+# NOTE: AWS の環境名は文字数を揃えると良い
+# 一般的には dev, stage, prod がよく使われるが、個人的には dev, stg, prd を採用したい
+# AWS に限らず IaaS 系のリソースは命名時の文字数制限が短い傾向がある
+# リソース名に環境名を含める場合、 "dev" では問題ないが "prod" は制限に達するケースがある
+variable "environment" {
+  default     = "dev"
+  type        = string
+  description = "デプロイ先の環境 {dev|stg|prd}"
+}
+
+variable "dns_zone_id" {
+  default     = "Z1234567890ABCDEFGHIJ"
+  type        = string
+  description = "ドメインの Route 53 ホストゾーン ID"
+}
+
+# ******************************************************************************
+# 固有変数
+# ******************************************************************************
+
+variable "db_name" {
+  default     = "kawasawa"
+  type        = string
+  description = "データベースの名称"
+}
+
+variable "db_port" {
+  default     = 3306
+  type        = number
+  description = "データベースのポート番号"
+}
+
+variable "app_port" {
+  default     = 3000
+  type        = number
+  description = "アプリケーションのポート番号"
+}
+
+# ******************************************************************************
+# 固有変数 (シークレット)
+# ******************************************************************************
+
+variable "db_username" {
+  type        = string
+  description = "データベースのユーザ名 (SECRET)"
+}
+
+variable "db_password" {
+  type        = string
+  description = "データベースのパスワード (SECRET)"
+  sensitive   = true
+}
