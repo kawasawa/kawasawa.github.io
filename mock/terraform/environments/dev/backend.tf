d8459924172941cf75bdc111319c8ba6aa321371
Kazuki Awasawa <16559662+kawasawa@users.noreply.github.com>
2022-12-01 00:00:00 +0900

    モックサーバの IaC 対応 (Terraform)

---
+++ mock/terraform/environments/dev/backend.tf
@@ -0,0 +1,31 @@
+terraform {
+  # 使用する Terraform のバージョンを指定
+  required_version = "1.4.7"
+
+  # 使用するプロバイダのバージョンを指定
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.77.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "4.0.4"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "2.4.1"
+    }
+  }
+
+  # tfstate の保管先を指定
+  backend "s3" {
+    key     = "terraform.tfstate"
+    encrypt = true
+
+    # 下記項目は実行時にシェルから引き渡す
+    # bucket         = ""
+    # dynamodb_table = ""
+    # region         = ""
+  }
+}
