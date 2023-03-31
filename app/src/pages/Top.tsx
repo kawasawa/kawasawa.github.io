930e23cfea167359814aef8cad1d35b149a11ade
Kazuki Awasawa <16559662+kawasawa@users.noreply.github.com>
2023-04-01 00:00:00 +0900

    フレームワーク導入 (React), モックサーバ構築 (Express)

---
+++ app/src/pages/Top.tsx
@@ -0,0 +1,26 @@
+import { Box } from '@mui/material';
+import React from 'react';
+
+import { About, Articles, Banner, Footer, Header, Products } from '@/components';
+import { BackToTopButton } from '@/components/controls';
+
+export const Top = () => {
+  const backToTopRef = React.useRef<HTMLDivElement>(null);
+  const isVisible = React.useCallback(
+    () => !!backToTopRef?.current && backToTopRef.current.offsetTop < window.scrollY,
+    []
+  );
+
+  return (
+    <Box sx={{ color: 'common.white', bgcolor: 'grey.900' }}>
+      <Header data-testid="Top__Header" />
+      <Banner data-testid="Top__Banner" />
+      <div ref={backToTopRef} />
+      <Products data-testid="Top__Products" />
+      <Articles data-testid="Top__Articles" />
+      <About data-testid="Top__About" />
+      <Footer data-testid="Top__Footer" />
+      <BackToTopButton isVisible={isVisible} data-testid="Top__BackToTopButton" />
+    </Box>
+  );
+};
