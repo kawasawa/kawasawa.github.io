a91405d7d057fba82060987cd6e78fe9ef759ebc
Kazuki Awasawa <16559662+kawasawa@users.noreply.github.com>
2023-11-01 00:00:00 +0900

    多言語対応

--- mock/docker/mysql/initdb.d/01_ddl.sql
+++ mock/docker/mysql/initdb.d/01_ddl.sql
@@ -6,8 +6,10 @@ SET FOREIGN_KEY_CHECKS=1;
 
 CREATE TABLE articles_metadata (
   id CHAR(20) NOT NULL,
-  title VARCHAR(255) NOT NULL,
-  body TEXT NOT NULL,
+  title_ja_jp VARCHAR(255) NOT NULL,
+  title_en_us VARCHAR(255) NOT NULL,
+  body_ja_jp TEXT NOT NULL,
+  body_en_us TEXT NOT NULL,
   tags VARCHAR(255) NOT NULL,
   url TEXT NOT NULL,
   likes_count INT NOT NULL,
