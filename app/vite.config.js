9fea545f2a5a33d1b415fa1570ad2054ccbcbfd0
Kazuki Awasawa <16559662+kawasawa@users.noreply.github.com>
2023-12-01 00:00:00 +0900

    PWA 対応

--- app/vite.config.js
+++ app/vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import envCompatible from 'vite-plugin-env-compatible';
+// import { VitePWA } from 'vite-plugin-pwa';
+
+// import manifest from './public/manifest.json';
 
 export default defineConfig({
   plugins: [
@@ -14,6 +17,14 @@ export default defineConfig({
       mountedPath: 'process.env',
       prefix: 'REACT_APP_',
     }),
+    // PWA 対応用プラグイン
+    // HACK: VitePWA によりサブディレクトリへのアクセスがルートにリダイレクトされるためコメントアウト
+    // 通常の開発では問題ないと思われるが、GitHub Pages で他の Web アプリをサブディレクトリにデプロイしており相性が悪い
+    //VitePWA({
+    //  registerType: 'autoUpdate',
+    //  includeAssets: ['favicon.ico'],
+    //  manifest,
+    //}),
   ],
   build: {
     outDir: 'build',
