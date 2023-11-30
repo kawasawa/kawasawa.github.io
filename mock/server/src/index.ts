5d3b1f03a1f052acab725ead7471c0f7ea0d050a
kawasawa <16559662+kawasawa@users.noreply.github.com>
2023-12-01 00:00:00 +0900

    ORM 移行 (Sequelize -> Prisma)

--- mock/server/src/index.ts
+++ mock/server/src/index.ts
@@ -2,7 +2,6 @@ import dotenv from 'dotenv';
 import fs from 'fs';
 
 import { createApp } from './app';
-import { initializeAdapter } from './db';
 
 const envName = process.env.NODE_ENV;
 if (!envName) throw new Error(`NODE_ENV is empty.`);
@@ -13,8 +12,6 @@ if (!fs.existsSync(envPath)) envPath = './.env';
 if (!fs.existsSync(envPath)) throw new Error(`EnvFile does not exists. : path="${envPath}"`);
 dotenv.config({ path: envPath });
 console.log(`EnvFile has been loaded. : path="${envPath}"`);
-
-initializeAdapter();
 
 const app = createApp();
 app.listen(process.env.EXPRESS_PORT, () => console.log(`Start to listen. : port="${process.env.EXPRESS_PORT}"`));
