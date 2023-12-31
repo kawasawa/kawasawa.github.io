c65fc30ac35136302b315c1575ccfd7a4b9c0ee4
Kazuki Awasawa <16559662+kawasawa@users.noreply.github.com>
2024-01-01 00:00:00 +0900

    静的コンテンツのAPI化

---
+++ mock/docker/mysql/initdb.d/dml/careers.sql
@@ -0,0 +1,7 @@
+INSERT INTO careers VALUES
+  (1, '2014-03', 'Yamanashi', '山梨大学 工学部 卒業', 'University of Yamanashi', 'https://www.yamanashi.ac.jp/favicon.ico', 'https://www.yamanashi.ac.jp/', true),
+  (2, '2014-04', 'Yamanashi', '株式会社 YSK e-com', 'YSK e-com Co., Ltd.', 'https://www.ysk.co.jp/favicon.ico', 'https://www.ysk.co.jp/', true),
+  (3, '2020-04', 'Yamanashi', '株式会社 コンピュータマインド', 'Computermind Corp.', 'https://www.compmind.co.jp/favicon.ico', 'https://www.compmind.co.jp/', false),
+  (4, '2020-11', 'Tokyo', '株式会社 ジェーシービー', 'JCB Co., Ltd.', 'https://www.jcb.co.jp/images/favicon.ico', 'https://www.jcb.co.jp/', true),
+  (5, '2023-06', 'Tokyo', '個人事業主として開業', 'Sole Proprietor', '', '', true)
+;
