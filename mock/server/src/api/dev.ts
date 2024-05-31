a22c4f63c8feee71282e7d01174867222209cf1a
Kazuki Awasawa <16559662+kawasawa@users.noreply.github.com>
2024-06-01 00:00:00 +0900

    ロガー移行 (morgan -> pino)

--- mock/server/src/api/dev.ts
+++ mock/server/src/api/dev.ts
@@ -1,40 +1,34 @@
 import boom from '@hapi/boom';
-import { NextFunction, Request, Response } from 'express';
+import { Request, Response } from 'express';
 
+import { withErrorHandler } from '../errors';
+import { info } from '../lib';
 import { ApiSuccessResponse } from '../responses';
 import { createSchema } from '../schemas/dev';
 
-export const error = () => {
+export const error = withErrorHandler(() => {
   throw new Error('dummy error');
-};
+});
 
-export const boomError = () => {
+export const boomError = withErrorHandler(() => {
   throw boom.internal('dummy boom error');
-};
+});
 
-export const asyncError = async (req: Request, res: Response, next: NextFunction) => {
-  // 非同期関数の例外は try-catch しないと捕捉できない
-  try {
-    throw new Error('dummy async error');
-  } catch (e) {
-    next(e);
-  }
-};
+export const asyncError = withErrorHandler(async () => {
+  throw new Error('dummy async error');
+});
 
-export const csrfToken = (req: Request, res: Response) => {
+export const csrfToken = withErrorHandler((req: Request, res: Response) => {
   // 本来の CSRF トークンは、サーバが生成し html ともに返却、クライアントがそれをレンダリングし form タグから同期的に POST されるのが一般的である
   // Laravel や Rails など多くのフルスタックフレームワークではこの機構が搭載されている
   // 一方 SPA の場合 Next.js の NextAuth.js では getServerSideProps を使いサーバサイドでトークンを取得しクライアントに引き渡し非同期で通信している
   // ただし Vercel の見解にあるように、モダンブラウザでは Origin ヘッダーと SameSite 属性で CSRF 攻撃を概ね防げるため、フレームワーク側で対策を実装しないケースも増えている
   // とはいえ、現段階では古いブラウザ等の存在を考慮するとまだ CSRF トークンが必要になる
   res.status(200).json({ success: true, data: req.csrfToken() } as ApiSuccessResponse);
-};
+});
 
-export const send = async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const params = await createSchema().validate(req.body);
-    res.status(200).json({ success: true, data: params } as ApiSuccessResponse);
-  } catch (e) {
-    next(e);
-  }
-};
+export const send = withErrorHandler(async (req: Request, res: Response) => {
+  const params = await createSchema().validate(req.body);
+  info(req, 'mail send', params);
+  res.status(200).json({ success: true, data: params } as ApiSuccessResponse);
+});
--- mock/server/src/errors.ts
+++ mock/server/src/errors.ts
@@ -1,6 +1,52 @@
 import boom from '@hapi/boom';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
+import { NextFunction, Request, RequestHandler, Response } from 'express';
 import { ValidationError } from 'yup';
+
+import { _fatal, error, warn } from './lib';
+
+/**
+ * 指定されたリクエストハンドラに対して、例外の発生と捕捉の共通化を行います。
+ * @param requestHandler リクエストハンドラ
+ * @returns 処理の共通化が行われたリクエストハンドラ
+ */
+export const withErrorHandler =
+  (requestHandler: RequestHandler) => async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      await requestHandler(req, res, next);
+    } catch (err) {
+      // エラーインスタンスを共通化する
+      const isManual = boom.isBoom(err);
+      const b = convertToBoomInstance(err);
+
+      // NOTE: REST API はステータスコードでエラーの発生源を判別する
+      //   (もちろん API が適切なステータスコードを設定することが前提である)
+      //   - 4xx: クライアント起因
+      //     - 401: 認証エラー
+      //     - 403: 認可エラー
+      //     - 404: リソースが存在しない
+      //     - 400: その他のエラー (バリデーションエラーなども含む)
+      //   - 5xx: サーバ起因
+      //     - 500: サーバ内エラー
+      // ログを発行する
+      if (!b.isServer) {
+        // クライアント起因の例外は WARN とする
+        warn(req, 'request failed. client-side error occurred.', b);
+      } else if (isManual) {
+        // 明示的に throw された例外は ERROR とする
+        error(req, 'request failed. server-side error occurred.', b);
+      } else {
+        // ハンドルされていない例外は FATAL とする
+        _fatal(req, 'request failed. unhandled error occurred.', b);
+      }
+
+      // レスポンスを返却する
+      // NOTE: 5xx エラーはセキュリティの観点でメッセージを隠蔽すべきである
+      //   本プロダクトでは boom が例外をラップし 5xx のメッセージを置き換えている
+      //   see: https://hapi.dev/module/boom/api/?v=10.0.1#http-5xx-errors
+      next(b);
+    }
+  };
 
 /**
  * エラーオブジェクトを Boom インスタンスに変換します。
