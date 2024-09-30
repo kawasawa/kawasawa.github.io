9225ed189b82bd826e9b8f7d190622ab0ae7bb15
Kazuki Awasawa <16559662+kawasawa@users.noreply.github.com>
2024-10-01 00:00:00 +0900

    ロガー移行 (morgan -> pino)

--- mock/server/src/api/dev.ts
+++ mock/server/src/api/dev.ts
@@ -1,27 +1,24 @@
 import boom from '@hapi/boom';
-import { NextFunction, Request, Response } from 'express';
+import { Request, Response } from 'express';
 
 import { cookieOptions, csrfConstants } from '../app';
+import { observeError } from '../errors';
+import { info } from '../logger';
 import { createSchema } from '../schemas/dev';
 
-export const error = () => {
+export const error = observeError(() => {
   throw new Error('dummy error');
-};
+});
 
-export const boomError = () => {
+export const boomError = observeError(() => {
   throw boom.internal('dummy boom error');
-};
+});
 
-export const asyncError = async (req: Request, res: Response, next: NextFunction) => {
-  // 非同期関数の例外は try-catch しないと捕捉できない
-  try {
-    throw new Error('dummy async error');
-  } catch (e) {
-    next(e);
-  }
-};
+export const asyncError = observeError(async () => {
+  throw new Error('dummy async error');
+});
 
-export const csrfToken = (req: Request, res: Response) => {
+export const csrfToken = observeError((req: Request, res: Response) => {
   // 本来の CSRF トークンは、サーバが生成し html ともに返却、クライアントがそれをレンダリングし form タグから同期的に POST されるのが一般的である
   // Laravel や Rails など多くのフルスタックフレームワークではこの機構が搭載されている
   // 一方 SPA の場合 Next.js の NextAuth.js では getServerSideProps を使いサーバサイドでトークンを取得しクライアントに引き渡し非同期で通信している
@@ -45,13 +42,10 @@ export const csrfToken = (req: Request, res: Response) => {
       httpOnly: false,
     })
     .json({ success: true });
-};
+});
 
-export const send = async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const params = await createSchema().validate(req.body);
-    res.status(200).json({ success: true, value: params });
-  } catch (e) {
-    next(e);
-  }
-};
+export const send = observeError(async (req: Request, res: Response) => {
+  const params = await createSchema().validate(req.body);
+  info(req, 'mail send', params);
+  res.status(200).json({ success: true, value: params });
+});
